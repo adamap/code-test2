038965ee2225135c84d78acffe70c9d0ec65b7a6
adamap <dzheng_box@hotmail.com>
2016-03-03 19:47:26 -0500

    .

---
+++ 279perfectsquares.cpp
@@ -0,0 +1,55 @@
+
+
+//
+//
+//Given a positive integer n, find the least number of perfect square numbers (for example, 1, 4, 9, 16, ...) which sum to n. 
+//
+//For example, given n = 12, return 3 because 12 = 4 + 4 + 4; given n = 13, return 2 because 13 = 4 + 9. 
+//
+//
+//
+//
+
+
+int numSquares_rec(int n)
+{
+    int res = n;
+    int num = 2;
+        
+    while(num*num <= n)
+    {
+        int a = n/(num*num);
+        int b = n%(num*num);
+            
+        res = min(res, a+numSquares_rec(b));
+        ++num;
+    }
+        
+    return res;
+}
+
+int numSquares_dp(int n)
+{
+    vector<int>dp(n+1, INT_MAX);
+    dp[0] = 0;
+    
+    for ( int i = 0; i <= n; i++)
+    {
+        for ( int j = 1; i+j*j <= n; j++)
+        {
+            dp[i+j*j] = min(dp[i+j*j], dp[i]+1);            
+        }
+    }
+    
+    return dp[n];
+}
+
+class Solution {
+public:
+    int numSquares(int n) 
+    {
+        //int ret = numSquares_rec(n);
+        int ret = numSquares_dp(n);
+        return ret;
+    }
+};
